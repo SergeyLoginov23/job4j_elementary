9355c58f4735bb55ef15a1c95d76bc51b6ea85ec
Sergey <Professional23@mail.ru>
2021-11-11 09:34:22 +0300

    Switch - matcher [#334071]

--- src/main/java/ru/job4j/condition/SwitchWeek.java
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -26,7 +26,7 @@ public class SwitchWeek {
                 name = "Воскресенье";
                 break;
             default:
-                name = "Ошибка";
+                name = "Ошибка!";
                 break;
         }
         return name;
