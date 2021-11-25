10e3cdd271e939dbf1658ce6893f00a2447ad5ef
Sergey <Professional23@mail.ru>
2021-11-25 19:29:04 +0300

    Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/loop/Symmetry.java
+++ src/main/java/ru/job4j/loop/Symmetry.java
@@ -2,10 +2,10 @@ package ru.job4j.loop;
 
 public class Symmetry {
     public static boolean check(int i) {
-        String s =  Integer.toString(i);
+        String str =  Integer.toString(i);
         boolean ret = true;
-        for (int index = 0; index < s.length() / 2; index++) {
-            if (s.charAt(index) !=  s.charAt(s.length() - 1 - index)) {
+        for (int index = 0; index < str.length() / 2; index++) {
+            if (str.charAt(index) !=  str.charAt(str.length() - 1 - index)) {
                 ret = false;
                 break;
             }
