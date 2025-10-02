4f1f60806433e4de0a98c69f7ba73cec6ce4e42f
Oklevet <nikstar1235@gmail.com>
2025-10-02 21:16:18 +0300

    6.4.0. Слово начинается с ... [#41585 #230408]

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -4,7 +4,7 @@ public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
         boolean etalon = data[0];
-        for (int index = 1; index < data.length  - 1; index++) {
+        for (int index = 1; index < data.length; index++) {
            if  (etalon != data[index]) {
                result = false;
                break;
